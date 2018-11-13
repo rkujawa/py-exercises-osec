552c634f72af5cd1923bcf484df90bd5af917b02
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2018-11-13 22:28:55 +0100

    Add solution to exercise 4.

--- ch02/ex04_employees_csv.py
+++ ch02/ex04_employees_csv.py
@@ -6,3 +6,28 @@
 - All programmers should additional have salaries increased by 500
 - Write out the modified database to nowe_dane.txt file in the same format (use join())
 """
+import csv
+
+csvf_read = open('dane.txt')
+reader = csv.reader(csvf_read)
+
+employees = []
+
+for rrow in reader:
+    employees.append(rrow)
+
+print(employees)
+
+for e in employees:
+    e[2] = int(e[2])
+    e[2] += e[2] * 0.1
+    if e[1] == "programista":
+        e[2] += 500
+
+print(employees)
+
+csvf_write = open('nowe_dane.txt', 'w')
+writer = csv.writer(csvf_write)
+
+for e in employees:
+    writer.writerow(e)
