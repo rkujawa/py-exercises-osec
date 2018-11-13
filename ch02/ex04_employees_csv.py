872bdf5295710cdcb502a151bf4d8124f4d2aed9
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2018-11-13 16:23:02 +0100

    Add exercise 4 text.

---
+++ ch02/ex04_employees_csv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+- Read dane.txt and built python data structures representing it (use split())
+- Print out all employees from database
+- Increase all employees salary by 10%
+- All programmers should additional have salaries increased by 500
+- Write out the modified database to nowe_dane.txt file in the same format (use join())
+"""
