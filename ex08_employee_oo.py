c7fb311340d7188c1619e80e540314faa5ab712d
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2018-11-15 10:16:21 +0100

    Add pydoc.

--- ex08_employee_oo.py
+++ ex08_employee_oo.py
@@ -11,21 +11,26 @@ also something that will allow representing object as string
 """
 
 class Employee:
+    """This is a class representing a single employee."""
     def __init__(self):
         self.name = ""
         self.surname = ""
         self.salary = 0
     def name_set(self, name):
+        """Set the (first) name of an employee."""
         self.name = name
     def surname_set(self, surname):
+        """Set the (last) name of an employee."""
         self.surname = surname
     def salary_set(self, salary):
+        """Set the salary of an employee."""
         if salary < 2000:
             # Throw exception ;p
             print("Nope")
         else:
             self.salary = salary
     def __str__(self):
+        """Return string representation of an employee (name and salary)."""
         return self.name + " " + self.surname + " zarabia " + str(self.salary)
 
 e1 = Employee()
